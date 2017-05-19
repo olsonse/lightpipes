f7b9bbc41433b59fc2ad45bd30f7c4d44e92644b
Spencer E. Olson <olsonse@umich.edu>
2017-05-19 08:54:36 -0600

    add py examples of using non-square optical field

---
+++ examples/python/non_square_field_blazed_grating.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+"""
+LightPipes for Octave Optical Toolbox
+
+This file demonstrates diffraction from a blazed grating where the optical field
+is not square, thus saving computational resources while still resolving the
+grating.
+"""
+
+from physical.unit import *
+from common import Propagate1D
+from lightpipes import Field, grating
+from pylab import *
+
+
+def main(return_final=False):
+  N               = r_[2**21,2]           # Number of pixels (different resolution for demo)
+  DX              = 60*mm                 # physical size in the important direction
+  wavelength      = 780*nm                # Wavelength
+  gaussian_size   = 1.0/sqrt(2)*mm       # 1/e Intensity width of beam; (note, it's not the 1/e^2)
+  L_propagate     = 25*mm                 # distance from initial lens to 2D-MOT
+
+  # grating parameters
+  grating_period  = mm/900
+  grating_lambda  = 1200*nm               # for the blazed grating
+
+
+  ######## END Param ##############
+
+  side_length     = DX * r_[1, N[1]/float(N[0])] # physical size
+
+
+  ####### Create grating
+  G = grating.phase.Blazed(grating_period/m, wavelength/m, grating_lambda/m)
+  ####### END Create grating
+
+
+  propagate = Propagate1D(N, side_length, window_size=511)
+
+  #####  With the knife edge between the SLM (at SLM focus) relay:1,lens:1.
+  #####  In this case, we relay the knife edge to the MOT and not the SLM plane.
+  F = Field( N, side_length/m, wavelength/m )   # beginning field
+  F.gaussian_aperture(gaussian_size/m, 1e300)
+
+  G.apply(F)
+
+  if return_final:
+    F.forvard(L_propagate/m)
+    plot( (F.value * F.value.conj()).real[:,N[1]/2] )
+    return F
+  else:
+    propagate(F, L_propagate/m, L_propagate/m/75.)
+
+
+  print 'press enter to exit'
+  raw_input()
+
+if __name__ == '__main__':
+  main()
